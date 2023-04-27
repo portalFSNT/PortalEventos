7b488db1fee596fd2604329a0ae0258691f4f1ef
lucastamura <lucas10.tamura@gmail.com>
2023-04-27 09:36:15 -0300

    adicionando tags da tela de login e cadastro

--- Front-end/portaleventos/src/app/app.module.ts
+++ Front-end/portaleventos/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { TypeLoginComponent } from './type-login/type-login.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    TypeLoginComponent
   ],
   imports: [
     BrowserModule

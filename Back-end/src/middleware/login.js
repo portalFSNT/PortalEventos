31abad6e90b05c1de29a5098e7ad4a7a46340119
luis-cavano <luissevilhadev@gmail.com>
2023-04-30 14:10:00 -0300

    feat: JWT verification and JTW update

--- Back-end/src/middleware/login.js
+++ Back-end/src/middleware/login.js
@@ -1,7 +1,6 @@
 var jwt = require('jsonwebtoken');
 
-exports.required = (req, res, next) => {
-
+exports.required = (req, res, next)=>{
     try {
         const token = req.headers.authorization.split(' ')[1];
         const decode = jwt.verify(token, process.env.JWT_KEY);
@@ -10,5 +9,31 @@ exports.required = (req, res, next) => {
     } catch (error) {
         return res.status(401).send({ mensagem: 'Falha na autenticação midlleware' });
     }
-
 }
+
+// exports.verifyToken = (req, res, next) => {
+//     const authHeader = req.headers['authorization'];
+//     const token = authHeader && authHeader.split(' ')[1];
+
+//     if (token == null) return res.sendStatus(401);
+
+//     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+//         console.log(err);
+//         if (err) return res.sendStatus(403);
+//         req.user = user;
+//         next();
+//     })
+// }
+
+
+// exports.verifyRefreshToken = (req, res, next) => {
+//     const {refreshToken} = req.body;
+//     if (refreshToken == null) return res.sendStatus(401);
+
+//     jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, (err, user) => {
+//         console.log(err);
+//         if (err) return res.sendStatus(403);
+//         req.user = user;
+//         next();
+//     })
+// }

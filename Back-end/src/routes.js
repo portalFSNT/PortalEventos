9a1357a715c6b2fbaa09fc75472c2a68aa624fa7
luis-cavano <luissevilhadev@gmail.com>
2023-04-30 14:01:33 -0300

    add Convidado routes

--- Back-end/src/routes.js
+++ Back-end/src/routes.js
@@ -3,7 +3,17 @@ const router = express.Router();
 //const multer = require('multer');
 const login = require('./middleware/login');
 
-const UserController = require("./controllers/UserControllers")
+//USER -----   
+const UserController = require("./controllers/UserControllers");
 router.post('/login', UserController.login);
 router.get('/login', UserController.login); 
+
+//CONVIDADO -----
+const ConvidadoController = require("./controllers/ConvidadoControllers");
+router.get("/convidados", ConvidadoController.getAll);
+router.get("/convidados/:nome", ConvidadoController.getById);
+router.post("/convidados/", ConvidadoController.addConvidado);
+router.put("/convidados/:nomes", ConvidadoController.updateConvidado);
+router.delete("/convidados/:nome", ConvidadoController.delConvidado);
+
 module.exports = router;

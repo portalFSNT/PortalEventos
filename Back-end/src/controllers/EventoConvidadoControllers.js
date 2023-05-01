27f6cec84f237bfc7de05adbfc4a9f1d0396710a
luis-cavano <luissevilhadev@gmail.com>
2023-05-01 13:21:16 -0300

    feat: getAll function

--- Back-end/src/controllers/EventoConvidadoControllers.js
+++ Back-end/src/controllers/EventoConvidadoControllers.js
@@ -1,5 +1,24 @@
 const EventoConvidadoService = require("../services/EventoConvidadoService");
 
 module.exports = {
+    getAll: async(req, res) => {
+        let json = { error: "", result: [] };
+        let evento_convidados = await EventoConvidadoService.getAll();
 
+        for (let i in evento_convidados) {
+          json.result.push({
+            data_hora:evento_convidados[i].data_hora,
+            descricao:evento_convidados[i].descricao,
+            nome:evento_convidados[i].nome,
+            cargo:evento_convidados[i].cargo,
+            empresa:evento_convidados[i].empresa,
+            telefone:evento_convidados[i].telefone,
+            condicao: evento_convidados[i].condicao,
+            anunciados: evento_convidados[i].anunciado,
+            presenca: evento_convidados[i].presenca,                        
+          });
+        }
+        res.status(200);
+        res.json(json);
+    },
 }
--- Back-end/src/services/EventoConvidadoService.js
+++ Back-end/src/services/EventoConvidadoService.js
@@ -1,5 +1,17 @@
 const db = require("../db");
 
 module.exports = {
-
+    getAll: () => {
+        return new Promise((acepted, rejected) => {
+            db.query(`SELECT e.descricao, e.data_hora, c.nome, c.email, c.cargo,c.telefone,ec.condicao,ec.anunciado,ec.presenca  
+            FROM evento_convidado ec, convidado c, evento_presenca e 
+            WHERE ec.id_presenca = e.id AND ec.id_convidado=c.id;`,(error, results)=>{
+                if(error){
+                    rejected(error);
+                    return;
+                }
+                acepted(results);
+            });
+        });
+    },
 }

6848b501bd5403de700fa9c8b6dea307a93a2ea6
diogo-gb <diogoiwamoto7999@gmail.com>
2023-05-04 08:14:21 -0300

    Front atualizado  04/05

--- Front-end/portaleventos/src/app/app-router.module.ts
+++ Front-end/portaleventos/src/app/app-router.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { TypeLoginComponent } from './type-login/type-login.component';
+import { HeaderComponent } from './header/header.component';
+
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'type-register', component: TypeLoginComponent }
+  { path: 'type-register', component: TypeLoginComponent },
+  { path: 'header', component: HeaderComponent}
 ];
 
 @NgModule({

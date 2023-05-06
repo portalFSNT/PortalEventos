abe74916f93b7a136bdaa3a95e19edcc541515ff
luis-cavano <luissevilhadev@gmail.com>
2023-05-06 15:29:39 -0300

    fix: convidado routes updated

--- Back-end/src/routes.js
+++ Back-end/src/routes.js
@@ -13,7 +13,7 @@ router.delete('/user/:email', UserController.delUser);
 router.post('/login', UserController.login);
 
 //CONVIDADO -----
-const ConvidadoController = require("./controllers/ConvidadoControllers");
+const ConvidadoController = require("./controllers/convidado/ConvidadoControllers");
 router.get("/convidados", ConvidadoController.getAll);
 router.get("/convidados/:nome", ConvidadoController.getById);
 router.post("/convidados/", ConvidadoController.addConvidado);
@@ -21,7 +21,7 @@ router.patch("/convidados/:nomes", ConvidadoController.updateConvidado);
 router.delete("/convidados/:nome", ConvidadoController.delConvidado);
 
 //EMPRESA -----
-const EmpresaController = require("./controllers/EmpresaControllers");
+const EmpresaController = require("./controllers/convidado/EmpresaControllers");
 router.get("/empresas", EmpresaController.getAll);
 router.get("/empresas/:id_empresa", EmpresaController.getById);
 router.post("/empresas", EmpresaController.addEmpresa);
@@ -29,7 +29,7 @@ router.patch("/empresas/:empresa", EmpresaController.updateEmpresa);
 router.delete("/empresas/:empresa", EmpresaController.delEmpresa);
 
 //EVENTO -----
-const EventoController = require("./controllers/EventoControllers");
+const EventoController = require("./controllers/convidado/EventoPresencaControllers");
 router.get("/evento", EventoController.getAll);
 router.get("/evento/:id_evento", EventoController.getById);
 router.post("/evento", EventoController.addEvento);
@@ -37,7 +37,7 @@ router.patch("/evento/:id_evento", EventoController.updateEvento);
 router.delete("/evento/:id_evento", EventoController.delEvento);
 
 //EVENTO_CONVIDADO -----
-const EventoConvidadoController = require("./controllers/EventoConvidadoControllers");
+const EventoConvidadoController = require("./controllers/convidado/EventoConvidadoControllers");
 router.get("/evento_convidado", EventoConvidadoController.getAll);
 router.get("/evento_convidado/:id_evento", EventoConvidadoController.getById);
 router.post("/evento_convidado/:id_evento", EventoConvidadoController.addEventoConvidado);

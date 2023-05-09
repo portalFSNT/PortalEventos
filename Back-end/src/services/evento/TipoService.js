6e7c86886ed72168e618cdd9c86e4f2197028faa
luis-cavano <luissevilhadev@gmail.com>
2023-05-09 09:29:00 -0300

    feat: delTipo service function

--- Back-end/src/services/evento/TipoService.js
+++ Back-end/src/services/evento/TipoService.js
@@ -2,16 +2,29 @@ const db = require('../../db');
 
 module.exports = { 
     addTipo: (tipo) => {
-        db(`INSERT INTO tipo (tipo) VALUES (?)`,[tipo],(error, results) => {
-            if(error) { rejected(error); return;}
-            acepted(results);
+        return new Promise((acepted, rejected) => {
+            db.query(`INSERT INTO tipo (tipo) VALUES (?)`,[tipo],(error, results) => {
+                if(error) { rejected(error); return;}
+                acepted(results);
+            });
         });
     },
 
     updateTipo: (id) => {
-        db(`UPDATE tipo SET tipo =? WHERE id =?`,[id],(error, results) => {
-            if(error) { rejected(error); return;}
-            acepted(results);
+        return new Promise((acepted, rejected) => {
+            db.query(`UPDATE tipo SET tipo =? WHERE id =?`,[id],(error, results) => {
+                if(error) { rejected(error); return;}
+                acepted(results);
+            });
+        });
+    },
+
+    delTipo: (id) => {
+        return new Promise((acepted, rejected) => {
+            db.query(`DELETE FROM tipo WHERE id =?`,[id],(error, results) => {
+                if(error) { rejected(error); return;}
+                acepted(results);
+            });
         });
     },
 }

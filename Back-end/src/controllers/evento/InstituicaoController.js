7f7dfd5d2792dd5670f2101a411b76f4af703726
luis-cavano <luissevilhadev@gmail.com>
2023-05-18 12:48:11 -0300

    feat: updateInstituicao and delInstituicao functions

--- Back-end/src/controllers/evento/InstituicaoController.js
+++ Back-end/src/controllers/evento/InstituicaoController.js
@@ -7,7 +7,7 @@ module.exports = {
 
         try {
             let instituicoes = await InstituicaoService.getAll();
-            
+
             if(instituicoes){
                 json.results = instituicoes;
             } 
@@ -16,6 +16,23 @@ module.exports = {
             json.results = {error: error}
         }
         res.json(json); 
+    },
+
+    getById: async (req, res) => {
+        let json = { error:'', result:[] }
+        let id = req.params.id;
+
+        try {
+            let instituicao = await InstituicaoService.getById(id);
+            
+            if(instituicao){
+                json.result = { instituicao };    
+            }
+            
+        } catch (error) {
+            json.result = { error: error }
+        }
+        res.json(json);
     },
 
     addInstituicao: async(req, res) => {
@@ -35,5 +52,36 @@ module.exports = {
             res.status(400);
             res.json({error:"Campo(s) não enviado(s) ou inválido(s)"});
         }
+    },
+
+    updateInstituicao: async(req, res) => {
+        let json = { error:'', result:[] }
+
+        let id = req.params.id;
+        let nome = req.body.nome; 
+
+        try {
+            await InstituicaoService.updateInstituicao(id, nome);
+            json.result = { 
+                id, 
+                nome 
+            };
+        } catch (error) {
+            json.error(error);
+        }
+        res.json(json);
+    },
+
+    delInstituicao: async(req,res) => {
+        let json = { error:'', result:[] }
+
+        let id = req.params.id;
+
+        try {
+            await InstituicaoService.delInstituicao(id);
+        } catch (error) {
+            json.error(error);
+        }
+        res.json(json);
     }
 }
--- Back-end/src/services/evento/InstituicaoService.js
+++ Back-end/src/services/evento/InstituicaoService.js
@@ -11,11 +11,42 @@ module.exports = {
         });
     },
 
+    getById: (id) => {
+        return new Promise((acepted, rejected) => {
+            db.query(`SELECT id, nome FROM instituicao WHERE id=?`,[id], (err, result) => {
+                if(err){ rejected(err); return;}
+                if(result.length > 0) {
+                    acepted(result[0]);
+                }else{
+                    acepted(false);
+                }
+            });
+        });
+    },
+
     addInstituicao: ( nome ) => {
         return new Promise((acepted, rejected) => {
             db.query(`INSERT INTO instituicao (nome) VALUES (?)`,[nome], (err, result) => {
                 if(err){ rejected(err); return; }
                 acepted( result ); 
+            });
+        });
+    },
+
+    updateInstituicao: ( id, nome ) => {
+        return new Promise((acepted, rejected) => {
+            db.query(`UPDATE instituicao SET nome =? WHERE id =?`, [nome, id], (err, result) =>{
+                if(err){ rejected(err); return; }
+                acepted( result );
+            });
+        });
+    },
+
+    delInstituicao: (id) => {
+        return new Promise((acepted, rejected) => {
+            db.query(`DELETE FROM instituicao WHERE id=?`,[id], (err, result) => {
+                if(err){ rejected(err);  return; }
+                acepted(result);
             });
         });
     }

c95607a764fffe2e67809a68cb780bb95c6b60b2
luis-cavano <luissevilhadev@gmail.com>
2023-05-28 12:16:00 -0300

    feat: authentication file and functions added.

---
+++ Front-end/src/app/Portal/authentication/authentication.service.ts
@@ -0,0 +1,32 @@
+import { environment } from "src/environments/environment";
+import { UserService } from "./user/user.service";
+import { Injectable, } from "@angular/core";
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { Observable, tap } from "rxjs";
+
+const API = environment.API;
+@Injectable({
+    providedIn: "root",
+})
+export class AuthService {
+    constructor(
+        private HttpClient: HttpClient,
+        private userService: UserService
+    ){}
+
+    doLogin(email: string, senha:string): Observable<HttpResponse<any>>{
+        return this.HttpClient.post(
+            `${API}/login`,
+            {
+                email: email,
+                senha: senha,
+            },
+            { observe: 'response'}
+        ).pipe(
+            tap((res: any) =>{
+                const authToken= res.body!.token ?? '';
+                this.userService.savToken(authToken); 
+            })
+        )
+    }
+}
---
+++ Front-end/src/app/Portal/authentication/token.service.ts
@@ -0,0 +1,29 @@
+import { Token } from "@angular/compiler";
+import { Injectable } from "@angular/core";
+
+const KEY = 'token';
+
+@Injectable({
+    providedIn: "root"
+})
+export class TokenService {
+
+    constructor(){}
+
+    returnToken(){
+        return localStorage.getItem(KEY) ?? '';
+    }
+
+    saveToken(token: string){
+        localStorage.setItem(KEY, token);
+        console.log(token);
+    }
+
+    deleteToken(){
+        localStorage.removeItem(KEY);
+    }
+
+    haveToken(){
+        return !!this.returnToken();
+    }
+}

a4ce167c63bbd78cb3e798d1e1bf71746230ddef
luis-cavano <luissevilhadev@gmail.com>
2023-05-28 09:50:33 -0300

    *

--- Front-end/src/app/Portal/login/login.component.ts
+++ Front-end/src/app/Portal/login/login.component.ts
@@ -1,14 +1,22 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  constructor(private router: Router) { }
-  
+
+  form = new FormGroup({
+    email: new FormControl(null, Validators.required),
+    senha: new FormControl(null, Validators.required),
+  });
+
+  constructor(private router:Router) {}
+
+  ngOnInit(): void{}
 
   irParaTipoCadastro() {
     this.router.navigate(['/type-register']);

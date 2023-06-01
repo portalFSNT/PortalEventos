fda78d65cb5630835965f5a1029b6224d536f856
lucastamura <lucas10.tamura@gmail.com>
2023-06-01 08:37:54 -0300

    feat: listar eventos

--- Front-end/src/app/Eventos/home-eventos/evento.service.ts
+++ Front-end/src/app/Eventos/home-eventos/evento.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { TokenService } from '../../authentication/token.service';
 
 const API = environment.API;
 
@@ -9,12 +10,13 @@ const API = environment.API;
   providedIn: 'root'
 })
 export class EventoService {
+  constructor(private tokenService : TokenService, private http: HttpClient) { }
 
   private readonly  API = `${API}/events`;
+  private header = new HttpHeaders().set('Authorization', `Bearer ${this.tokenService.returnToken()}`);
 
-  constructor(private http: HttpClient) { }
 
   listarEvento():Observable<any>{
-    return this.http.get<any>(this.API)
+    return this.http.get<any>(this.API, { headers: this.header})
   }
 }

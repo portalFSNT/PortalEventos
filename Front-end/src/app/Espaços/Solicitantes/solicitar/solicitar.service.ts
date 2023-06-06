30a15ffff46100b753f93978221b25a17e1fd94b
Gabriel Calil <gabrielbcalil@gmail.com>
2023-06-06 09:58:20 -0300

    fix: get userId

--- Front-end/src/app/Espaços/Solicitantes/solicitar/solicitar.service.ts
+++ Front-end/src/app/Espaços/Solicitantes/solicitar/solicitar.service.ts
@@ -2,7 +2,8 @@ import { TokenService } from 'src/app/authentication/token.service';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { Observable } from 'rxjs';
+import { Observable, switchMap, take } from 'rxjs';
+import { UserService } from 'src/app/authentication/user/user.service';
 
 const API = environment.API;
 
@@ -12,12 +13,28 @@ const API = environment.API;
 export class SolicitarService {
   private readonly API_SolicitarAgendamento=`${API}/solicitacao`;
   private readonly API_BuscarEspacos=`${API}/espacos/`;
+  //header: HttpHeaders | { [header: string]: string | string[]; } | undefined;
 
-  constructor(private http: HttpClient, private tokenService : TokenService) { }
+  constructor(private http: HttpClient, private tokenService : TokenService, private userService: UserService) { }
 
   private header = new HttpHeaders().set('Authorization', `Bearer ${this.tokenService.returnToken()}`);
 
+  private getHeader(): HttpHeaders {
+    const token = this.tokenService.returnToken();
+    let userId: number | undefined = this.userService.returnUser().value.id;
+    userId = userId !== undefined ? userId : 0;
+  
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`).set('X-User-ID', userId.toString());
+  }
+
   solicitar(espacoSelecionado:number, data:string, horario_entrada:string, horario_saida:string, descricao:string):Observable<any>{
+
+    const header = this.getHeader();
+
+    return this.userService.returnUser().pipe(
+      take(1),
+      switchMap(user => {
+        const userId = user.id;
 
     return this.http.post<any>(this.API_SolicitarAgendamento, {
       status_solicitacao: 1,
@@ -29,9 +46,11 @@ export class SolicitarService {
       hora_termino: horario_saida, 
       descricao: descricao,
       id_espaco: espacoSelecionado,              
-      id_usuario: 6
+      id_usuario: userId
       }, {headers: this.header})
-  }
+  })
+  );
+}
 
   listarEspacos():Observable<any>{
   

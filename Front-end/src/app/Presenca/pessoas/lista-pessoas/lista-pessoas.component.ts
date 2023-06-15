5e97c2b6e50b3c2df78bbb27be0f768ed8793139
luis-cavano <luissevilhadev@gmail.com>
2023-06-15 15:11:18 -0300

    fix: adaptating for update

--- Front-end/src/app/Presenca/pessoas/lista-pessoas/lista-pessoas.component.ts
+++ Front-end/src/app/Presenca/pessoas/lista-pessoas/lista-pessoas.component.ts
@@ -1,3 +1,4 @@
+import { TestBed } from '@angular/core/testing';
 import { PessoaService } from './../pessoa.service';
 import { EditarPessoaComponent } from './../editar-pessoa/editar-pessoa.component';
 import { NovaPessoaComponent } from './../nova-pessoa/nova-pessoa.component';
@@ -5,8 +6,8 @@ import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
 
 import { Pessoa } from "../pessoa";
-import { Component, OnInit } from "@angular/core";
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { Component, Input,OnInit } from "@angular/core";
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: "app-lista-pessoas",
@@ -14,7 +15,11 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
   styleUrls: ["./lista-pessoas.component.scss","../../navbar-adm.scss"],
 })
 export class ListaPessoasComponent implements OnInit {
+  
+  @Input() pessoa: any;
+
   listaPessoa: Pessoa[] = [];
+  
   constructor(
     private service: PessoaService,
     private modalController:ModalController,
@@ -37,29 +42,58 @@ export class ListaPessoasComponent implements OnInit {
     await modal.present();
   }
 
-  async edit(
-   
-    nome:any,
-    cargo:any,
-    empresa:any,
-    email:any,
-    telefone:any
-    ){
- 
-      const modal=await this.modalController.create({
-        component:EditarPessoaComponent,
-        componentProps:{
-       
+  // async edit(pessoa: any){
+  //   const id = pessoa.id;
+  //   const nome = pessoa.nome;
+  //   const email = pessoa.email;
+  //   const cargo = pessoa.cargo;
+  //   const empresa = pessoa.empresa;
+  //   const id_empresa = pessoa.id_empresa
+
+  //   const modal=await this.modalController.create({
+  //       component:EditarPessoaComponent,
+  //       componentProps:{
+  //         id,
+  //         nome,
+  //         email,
+  //         cargo,
+  //         empresa,
+  //         id_empresa,
+  //       },
+  //       cssClass:"modal",
+  //     });
+  //     await modal.present();
+  //   }
+  
+  updatePessoa(pessoa: any){
+    console.log(pessoa.id)
+    const id = pessoa.id;
+    const nome = pessoa.nome;
+    const email = pessoa.email;
+    const cargo = pessoa.cargo;
+    const telefone = pessoa.telefone;
+    const empresa = pessoa.empresa;
+    const id_empresa = pessoa.id_empresa
+
+    const initialState: ModalOptions = {
+      initialState: {
+        list: [
+          id,
           nome,
-          cargo,
-          empresa,
           email,
-          telefone
-        },
+          cargo,
+          telefone,
+          empresa,
+          id_empresa,
+        ],
+        title: 'Modal update pessoa',
         cssClass:"modal",
-      });
-      await modal.present();
-    }
+      }
+    };
+    this.bsModalRef = this.modalService.show(EditarPessoaComponent, initialState);
+    this.bsModalRef.content.closeBtnName ='Close';
+  }
+
   delet(nome:any){
     this.service.delet(nome).subscribe(
       ()=>{
